7168f77c930f43628b65ea863b98bcb1c913218e
Leland <leland.d.moy@gmail.com>
2024-07-08 15:28:04 -0500

    dynamic routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Home() {
   return (
     <section className='py-24'>
       <div className='container'>
-        <h1 className='text-3xl font-bold'>Hello World</h1>
+        <h1 className='text-3xl font-bold'>Welcome to AdepT.dev</h1>
+        <h1>
+          Read <Link href="/about"> about me!</Link>
+        </h1>
+        <h1>
+          Read <Link href="/blogs"> blogs by me!</Link>
+        </h1>
+        <h1>
+          Buy <Link href="/products"> products!</Link>
+        </h1>
       </div>
     </section>
   )
