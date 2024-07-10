a941811af4792291c024a17b13d81f5547bb44e9
Leland <leland.d.moy@gmail.com>
2024-07-10 15:05:47 -0500

    dev

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import HeroSection from './components/HeroSection'
 
 export default function Home() {
   return (
-    <section className='py-24'>
-      <div className='container'>
-        <h1 className='text-3xl font-bold'>Hello World</h1>
-      </div>
-    </section>
+    <main className = "flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4">
+      {/* <h1 className="text-white">Welcome to AdepT.dev</h1> */}
+      <HeroSection/>
+    </main>
   )
 }
