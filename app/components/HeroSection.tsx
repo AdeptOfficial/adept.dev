172fee0dc0b84c257336b31bfa4e26b1f9f01542
Leland <leland.d.moy@gmail.com>
2024-07-12 17:11:31 -0500

    slight change to text

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
     <section>
       <div className='grid grid-cols-1 lg:grid-cols-12'>
         <div className='col-span-7 place=self-center'>
-          <h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'> Hello, I'm AdepT </h1>
+          <h1 className='text-white mb-4 text-6xl lg:text-8xl font-extrabold'> Hello, I'm AdepT </h1>
           <p className='text-[#ADB7BE] text-lg lg:text-xl'> hello world, This is plain text, hello again, please say hi back lol
           </p>
         </div>
