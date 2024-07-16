ef9332822a391bc1622e91ac008ec10a76468d36
Leland <leland.d.moy@gmail.com>
2024-07-16 05:01:43 -0500

    finally learnt client/server side rendering

---
+++ app/components/DiscordProfile.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+
+const DiscordProfile: React.FC = () => {
+  const [profilePicUrl, setProfilePicUrl] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const getProfilePic = async () => {
+      try {
+        const res = await fetch('/api/discord/discordProfilePicture');
+        if (!res.ok) {
+          throw new Error('Failed to fetch profile picture');
+        }
+        const data = await res.json();
+        console.log('Fetched data:', data);
+        setProfilePicUrl(data.profilePicUrl);
+      } catch (error: any) {
+        console.error('Error fetching profile picture:', error);
+        setError(error.message);
+      }
+    };
+
+    getProfilePic();
+  }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <div>
+      {profilePicUrl ? (
+        <Image src={profilePicUrl} alt="Discord Profile" width={100} height={100} />
+      ) : (
+        <p className='text-white'>Loading profile picture...</p>
+      )}
+    </div>
+  );
+};
+
+export default DiscordProfile;
---
+++ pages/api/discord/discordProfilePicture.ts
@@ -0,0 +1,30 @@
+// pages/api/discordProfilePicture.ts
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const discordToken = process.env.DISCORD_TOKEN; // Example of using a server-side env variable
+
+  if (!discordToken) {
+    return res.status(500).json({ error: 'Server-side environment variable not set' });
+  }
+
+  try {
+    const response = await fetch('https://discord.com/api/users/@me', {
+      headers: {
+        Authorization: `Bot ${discordToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch profile picture');
+    }
+
+    const data = await response.json();
+    console.log('API Response Data:', data); // Log the API response for debugging
+    const profilePicUrl = `https://cdn.discordapp.com/avatars/${data.id}/${data.avatar}.png`;
+
+    res.status(200).json({ profilePicUrl });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
