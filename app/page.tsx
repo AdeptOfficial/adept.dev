e19ff1cac2baed84a20096847cbdf7a3d758f099
Leland Moy <48741579+AdeptOfficial@users.noreply.github.com>
2025-04-01 15:15:58 -0500

    added svg

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import HeroSection from "./components/HeroSection";
-import DiscordProfile from './components/DiscordProfile';
 import React from 'react';
 
 export default function Home() {
   return (
     <main className = "flex min-h-screen min-w-full flex-col bg-[#121212] container mx-auto px-12 py-4">
-      {/* <h1 className="text-white">Welcome to AdepT.dev</h1> */}
+      {/* TODO: Add nav */}
       <HeroSection/>
-      {/* <DiscordProfile/> */}
+      {/* TODO: Add footer */}
     </main>
   )
 }
