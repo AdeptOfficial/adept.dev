62fbf087984b04977e47c3075fe661f0378e5cfd
Leland Moy <48741579+AdeptOfficial@users.noreply.github.com>
2025-04-01 15:38:56 -0500

    deployment ready

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'cdn.discordapp.com',
-            port: '',
-            pathname: '/**',
-          },
-        ],
-    }
-}
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
