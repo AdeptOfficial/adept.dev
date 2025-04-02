d9f45add36f431ebce1e15f9033cb22a4bade071
Leland Moy <Leland.d.moy@gmail.com>
2025-04-01 19:12:00 -0500

    Update redis.ts

--- lib/redis.ts
+++ lib/redis.ts
@@ -2,6 +2,6 @@
 import { Redis } from '@upstash/redis';
 
 export const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_REDIS_URL!,
-  token: process.env.UPSTASH_REDIS_REST_KV_REST_API_TOKEN!,
+  url: process.env.UPSTASH_REDIS_REST_URL!,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
 });
